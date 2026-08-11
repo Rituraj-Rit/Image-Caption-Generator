8a4b7e0926478f6952e5e1cf0ca3aa5f286d7f74
Rituraj Verma <riturajverma340@gmail.com>
2026-08-12 02:51:08 +0530

    feat: add web app manifest and favicon images for PWA support
    - Created site.webmanifest for the CaptionAI application with necessary metadata.
    - Added 192x192 and 512x512 PNG icons for the web app manifest.
    - Introduced index.html as the main entry point for the frontend, linking to the manifest and favicon.
    - Updated app.js to serve static files and handle all routes to return index.html.
    - Changed Vite configuration to proxy API requests to the production server.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ let express = require('express')
 let authRoutes = require('./routes/auth.route')
 let postRoutes = require('./routes/post.routes')
 let cookieParser = require('cookie-parser')
+let path = require('path')
 
 
 let app = express()
@@ -11,6 +12,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes)
 app.use('/api/posts', postRoutes);
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('*name', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 
 module.exports = app;
